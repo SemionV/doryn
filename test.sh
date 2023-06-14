ecc0d73600f7f5c5a052a62ab1198465c3e3ab46
VERIVOX\VarennikS <semen.varennik@verivox.com>
2023-06-14 13:23:43 +0200

    build project

--- test.sh
+++ test.sh
@@ -7,8 +7,8 @@ echo ${helloBuildFiles[*]}
 #compilation
 mkdir "$objDir" -p
 files=${helloBuildFiles[*]}
-echo "files to compile: ${files[*]}"
-for i in ${files[*]}; do \
+echo "files to compile: ${files}"
+for i in ${files}; do \
     echo "compiling: $i"; \
     fileName=$(basename -- $i)
     fileNameWithoutExtension=${fileName%.*}
