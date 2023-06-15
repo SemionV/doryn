261ad0a75c369b7dffcd44238861cd3bdf2f2bcc
VERIVOX\VarennikS <semen.varennik@verivox.com>
2023-06-15 22:04:58 +0200

    debug config

--- build/build-module.sh
+++ build/build-module.sh
@@ -5,7 +5,6 @@ targetDir=${3}
 projectDir="${project}/"
 buildDir="$(dirname "$0")/"
 targetDir="${targetDir:-${projectDir}bin/}"
-objDir="${projectDir}obj/"
 moduleFileExtensions=("cpp")
 
 #read named arguments of the script
@@ -17,9 +16,16 @@ while [ $# -gt 0 ]; do
         [[ $1 == "--linker-mode" ]]; then
         declare "linkerMode"="$2"
         shift
+    elif
+        [[ $1 == "--config" ]]; then
+        declare "config"="$2"
+        shift
     fi
     shift
 done
+
+config="${config:-release}"
+objDir="${projectDir}obj-${config}/"
 
 #validate script arguments
 #
@@ -40,7 +46,6 @@ function compileProject()
 
     objFiles=()
 
-    echo "Modules to compile: ${modules[*]}"
     echo "Compile with the following macroses defined: ${macroses[*]}"
     for module in ${modules[*]}; do 
         local regex="^${projectDir}(.*)\.${moduleFileExtensions[0]}" #regex to extract path to the module file relative to the project directory
@@ -51,16 +56,20 @@ function compileProject()
 
         objFiles+=($objFile) #collect names of all obj files in order to link them. this is a protection form linking deleted modules, as we do not clean up obj folder in order to save time for compilation
 
+        if [[ $config == "debug" ]]; then
+            compilerOptions="-g"
+        fi
+
         if [ $module -nt $objFile ]; then
             echo "Compile: $module";
-            g++ -c $module -o $objFile ${macroOptions[*]};
+            g++ ${compilerOptions} -c $module -o $objFile ${macroOptions[*]};
         else
             echo "Skip: $module";
         fi
     done;
 }
 
-echo "Compile project: ${project}"
+echo "Compile project: ${project}. Using config: ${config}"
 compileProject
 
 echo "Link project: ${project}"
