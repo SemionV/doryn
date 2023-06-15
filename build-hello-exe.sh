c8cc8cb81ed764c2cb768a2b0bd0e8d5e1ea4b9e
VERIVOX\VarennikS <semen.varennik@verivox.com>
2023-06-15 23:30:44 +0200

    Polishing makefile

--- build-hello-exe.sh
+++
@@ -1,3 +0,0 @@
-cd hello
-g++ -c "**.cpp"
-g++ -o "../bin/hello.exe" "**.o" -L. -l:"../mathLibrary/math_lib.a"
--- build-math-dll.sh
+++
@@ -1,2 +0,0 @@
-g++ -c -DMATHLIBRARY_EXPORTS "mathLibrary/**.cpp"
-g++ -shared -o "bin/math.dll" "**.o" -Wl,--out-implib,math_lib.a
