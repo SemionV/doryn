71c048426a2736ce07e47673db1632f32968a215
SemionV <semen.varennik@verivox.com>
2023-10-06 13:56:01 +0200

    Learning OpenGL

--- src/openGL/viewControllerOpenGL.h
+++ src/openGL/viewControllerOpenGL.h
@@ -12,7 +12,7 @@ namespace dory::openGL
             std::shared_ptr<domain::RepositoryReader<GlfwWindow>> windowRespository;
 
             enum VAO_IDs { Triangles, NumVAOs };
-            enum Buffer_IDs { ArrayBuffer, NumBuffers };
+            enum Buffer_IDs { ArrayBuffer, UniformBuffer, NumBuffers };
             enum Attrib_IDs { vPosition = 0 };
 
             GLuint  VAOs[NumVAOs];
@@ -86,18 +86,152 @@ namespace dory::openGL
                             });
                         glUseProgram(programId);
 
-                        auto triangleColorUniform = glGetUniformLocation(programId, "triangleColor");
-                        if(triangleColorUniform >= 0)
-                        {
-                            glUniform4f(triangleColorUniform, 0.7, 0.7, 0.7, 1.0);
-                        }
-
                         glVertexAttribPointer(vPosition, 2, GL_FLOAT, GL_FALSE, 0, (void*)(0));
                         glEnableVertexAttribArray(vPosition);
+
+                        auto triangleColorIndex = glGetUniformLocation(programId, "triangleColor");
+                        if(triangleColorIndex != GL_INVALID_INDEX)
+                        {
+                            glUniform4f(triangleColorIndex, 0.7, 0.7, 0.7, 1.0);
+                        }
+
+                        auto uniformBlockTest = makeUniformBlock("ColorsBlock", "brightColor", "hippieColor", "darkColor");
+
+                        const char* memberNames[] = {"ColorsBlock.brightColor", "ColorsBlock.hippieColor", "ColorsBlock.darkColor"};
+                        UniformBlock<3> colorsUniformBlock("ColorsBlock");
+
+                        bindUniformBlock(programId, uniformBlockTest, memberNames);
+
+                        auto colorsBlockIndex = glGetUniformBlockIndex(programId, "ColorsBlock");
+                        if(colorsBlockIndex != GL_INVALID_INDEX)
+                        {
+                            glBindBuffer( GL_UNIFORM_BUFFER, Buffers[UniformBuffer] );
+                            GLint blockSize {0};
+                            glGetActiveUniformBlockiv(programId, colorsBlockIndex, GL_UNIFORM_BLOCK_DATA_SIZE, &blockSize);
+
+                            GLfloat  colors[3][4] = {
+                            { 0.7f, 0.7f, 0.7f, 1.0f }, 
+                            { 0.7f, 0.7f, 0.0f, 1.0f },
+                            { 0.2f, 0.2f, 0.2f, 1.0f }};
+
+                            glBufferStorage( GL_UNIFORM_BUFFER, sizeof(colors), colors, 0);
+
+                            glBindBufferBase(GL_UNIFORM_BUFFER, colorsBlockIndex, Buffers[UniformBuffer]);
+
+                        }
                     }
                 }
 
                 return true;
+            }
+
+            struct Buffer
+            {
+                GLuint index = GL_INVALID_INDEX;
+                GLuint size = 0;
+                GLvoid* data = nullptr;
+            };
+
+            struct Uniform
+            {
+                std::string key;
+                GLuint index = GL_INVALID_INDEX;
+                GLint size = 0;
+                GLint offset = 0;
+                GLint type = 0;
+            };
+
+            template<unsigned int membersCount>
+            struct UniformBlock
+            {
+                std::string key;
+                GLuint index = GL_INVALID_INDEX;
+                GLint size = 0;
+                GLint bufferOffset = 0;
+                std::array<Uniform, membersCount> members;
+
+                template<typename TKey>
+                UniformBlock(TKey&& blockName):
+                    key(std::forward<TKey>(blockName))
+                {}
+            };
+
+            template<typename TName, typename... Ts>
+            decltype(auto) makeUniformBlock(TName blockName, Ts... memberNames)
+            {
+                std::initializer_list<TName> nameList = {memberNames...};
+                UniformBlock<sizeof...(Ts)> uniformBlock(std::forward<TName>(blockName));
+
+                auto current = nameList.begin();
+                auto end = nameList.end();
+                int i = 0;
+                while(current != end)
+                {
+                    uniformBlock.members[i].key = std::string(std::string(blockName) + "." + std::string(*current));
+
+                    ++current;
+                    ++i;
+                }
+
+                return uniformBlock;
+            }
+
+            template<unsigned int membersCount>
+            std::vector<const char*> getUniformBlockMemberNames(const UniformBlock<membersCount>& block)
+            {
+                std::vector<const char*> memberNames(membersCount, nullptr);
+                
+                for(std::size_t i = 0; i < block.members.size(); ++i)
+                {
+                    memberNames.emplace_back(block.members[i].key.c_str());
+                }
+
+                return memberNames;
+            }
+
+            template<unsigned int membersCount>
+            void bindUniformBlock(GLuint programId, UniformBlock<membersCount>& block, const char* memberNames[membersCount])
+            {
+                block.index = glGetUniformBlockIndex(programId, block.key.c_str());
+                if(block.index != GL_INVALID_INDEX)
+                {
+                    glGetActiveUniformBlockiv(programId, block.index, GL_UNIFORM_BLOCK_DATA_SIZE, &block.size);
+                    
+                    GLuint memberIndices[membersCount];
+                    GLint memberOffset[membersCount];
+                    GLint memberSize[membersCount];
+                    GLint memberType[membersCount];
+
+                    auto memberNames2 = getUniformBlockMemberNames(block);
+
+                    glGetUniformIndices(programId, membersCount, memberNames2.data(), memberIndices);
+                    glGetActiveUniformsiv(programId, membersCount, memberIndices, GL_UNIFORM_OFFSET, memberOffset);
+                    glGetActiveUniformsiv(programId, membersCount, memberIndices, GL_UNIFORM_SIZE, memberSize);
+                    glGetActiveUniformsiv(programId, membersCount, memberIndices, GL_UNIFORM_TYPE, memberType);
+
+                    for(std::size_t i = 0; i < membersCount; ++i)
+                    {
+                        Uniform& member = block.members[i];
+                        //member.key = memberNames[i];
+                        member.index = memberIndices[i];
+                        member.offset = memberOffset[i];
+                        member.type = memberType[i];
+                        member.size = memberSize[i];
+                    }
+                }
+            }
+
+            Buffer allocateBuffer(GLint bufferSize)
+            {
+                Buffer buffer;
+                buffer.size = bufferSize;
+                buffer.data = malloc(bufferSize);
+                if(buffer.data == nullptr)
+                {
+                    buffer.size = 0;
+                }
+
+                return buffer;
             }
 
             void stop(domain::entity::IdType referenceId, TDataContext& context) override
--- src/testApp/configuration/shaders/triangles/triangles.frag
+++ src/testApp/configuration/shaders/triangles/triangles.frag
@@ -3,8 +3,15 @@
 out vec4 fColor;
 uniform vec4 triangleColor;
 
+uniform ColorsBlock
+{
+    vec4 brightColor;
+    vec4 hippieColor;
+    vec4 darkColor;
+} colors;
+
 void main()
 {
     //fColor = vec4(0.5, 0.4, 0.8, 1.0);
-    fColor = triangleColor;    
+    fColor = colors.hippieColor;    
 }
