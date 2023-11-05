e18b5d679592906fa5b572dd6e4b6b03320e2392
SemionV <semen.varennik@verivox.com>
2023-11-05 15:45:46 +0100

    Vertex Layout mapping

--- src/baseTests/typeMapping.cpp
+++ src/baseTests/typeMapping.cpp
@@ -20,54 +20,13 @@ struct TextureCoordinates
     int v {};
 };
 
-template<typename T>
-class MemberMapping
+enum class AttributeId
 {
-    public:
-        virtual ~MemberMapping() = default;
-
-        virtual std::size_t getSize() = 0;
-        virtual void writeTo(const T& object,  void* buffer) = 0;
+    position,
+    color,
+    textureCoordinates,
+    normal
 };
-
-template<typename T, typename M, M T::*m>
-class MemberPointerMapping: public MemberMapping<T>
-{
-    public:
-        std::size_t getSize() override
-        {
-            return sizeof(M);
-        }
-
-        void writeTo(const T& object, void* buffer) override
-        {
-            *static_cast<M*>(buffer) = object.*m;
-        }
-};
-
-class TypeMapping
-{
-    MemberPointerMapping<Point, decltype(Point::x), &Point::x> pointXMapping;
-};
-
-void testBody()
-{
-    MemberPointerMapping<Point, decltype(Point::x), &Point::x> pointXMapping;
-
-    Point point {3};
-
-    REQUIRE(pointXMapping.getSize() == sizeof(int));
-
-    Point buffer;
-
-    pointXMapping.writeTo(point, &buffer.x);
-    REQUIRE(buffer.x == 3);
-}
-
-TEST_CASE( "Type mapping test", "[.][typeMapping]" ) 
-{
-    testBody();
-}
 
 using AttributeIndex = unsigned short;
 using Byte = std::uint8_t;
@@ -76,36 +35,38 @@ template<typename TId, TId Id, typename T>
 struct Attribute
 {
     static constexpr TId id = Id;
-    using AttributeType = T;
+    using ValueType = T;
 };
 
-template <class... Ts>
+template <typename TId, class... Ts>
 struct Layout;
 
-template<>
-struct Layout<>
+template<typename TId>
+struct Layout<TId>
 {
 };
 
-template<typename T, typename... Ts>
-struct Layout<T, Ts...>: public Layout<Ts...>
+template<typename TId, typename T, typename... Ts>
+struct Layout<TId, T, Ts...>: public Layout<TId, Ts...>
 {
+    using ParentType = Layout<TId, Ts...>;
+
     static constexpr std::size_t getAttributeSize()
     {
-        return sizeof(typename T::AttributeType);
+        return sizeof(typename T::ValueType);
     }
 
     static constexpr std::size_t getSize()
     {
         if constexpr (sizeof...(Ts) > 0)
         {
-            return getAttributeSize() + Layout<Ts...>::getSize();
+            return getAttributeSize() + ParentType::getSize();
         }
 
         return getAttributeSize();
     }
 
-    template<typename TId, TId attributeId, std::size_t offset = 0>
+    template<TId attributeId, std::size_t offset = 0>
     static constexpr std::size_t getAttributeOffset()
     {
         if constexpr(T::id == attributeId)
@@ -118,7 +79,7 @@ struct Layout<T, Ts...>: public Layout<Ts...>
         }
         else if constexpr(sizeof...(Ts) > 1)
         {
-            return Layout<Ts...>::template getAttributeOffset<TId, attributeId, offset + getAttributeSize()>();
+            return ParentType::template getAttributeOffset<attributeId, offset + getAttributeSize()>();
         }
         else
         {
@@ -127,20 +88,18 @@ struct Layout<T, Ts...>: public Layout<Ts...>
     }
 };
 
-enum class AttributeId
-{
-    position,
-    color,
-    textureCoordinates,
-    normal
+template<AttributeId id, typename T>
+struct VertexAttribute: public Attribute<AttributeId, id, T>
+{    
 };
 
 struct VertexLayout
 {
-    using LayoutMap = Layout<Attribute<AttributeId, AttributeId::position, Point>, 
-        Attribute<AttributeId, AttributeId::color, Color>, 
-        Attribute<AttributeId, AttributeId::textureCoordinates, TextureCoordinates>, 
-        Attribute<AttributeId, AttributeId::normal, Point>>;
+    using LayoutMap = Layout<AttributeId,
+        VertexAttribute<AttributeId::position, Point>, 
+        VertexAttribute<AttributeId::color, Color>, 
+        VertexAttribute<AttributeId::textureCoordinates, TextureCoordinates>,
+        VertexAttribute<AttributeId::normal, Point>>;
 
     static constexpr std::size_t getSize() noexcept
     {
@@ -150,7 +109,7 @@ struct VertexLayout
     template<AttributeId Id, typename T>
     static void writeAttribute(const T& attribute, Byte* buffer)
     {
-        auto offset = LayoutMap::getAttributeOffset<AttributeId, Id>();
+        auto offset = LayoutMap::getAttributeOffset<Id>();
 
         std::cout << "Attribute: " << static_cast<unsigned int>(Id) << "; Offset: " << offset << ";" << std::endl;
     }
@@ -175,30 +134,6 @@ struct VertexLayout
         writeAttribute<AttributeId::normal>(normal, buffer);
     }
 };
-
-void writeVertexPositions(Point* positions, Color* colors, std::size_t count, Byte* buffer)
-{
-    Byte* cursor = buffer;
-    for(std::size_t i = 0; i < count; ++i)
-    {
-        const Point& position = positions[i];
-        const Color& color = colors[i];
-        
-        memcpy(cursor, &position.x, sizeof(position.x));
-        cursor += sizeof(position.x);
-        memcpy(cursor, &position.y, sizeof(position.y));
-        cursor += sizeof(position.y);
-        memcpy(cursor, &position.z, sizeof(position.z));
-        cursor += sizeof(position.z);
-
-        memcpy(cursor, &color.r, sizeof(color.r));
-        cursor += sizeof(color.r);
-        memcpy(cursor, &color.g, sizeof(color.g));
-        cursor += sizeof(color.g);
-        memcpy(cursor, &color.b, sizeof(color.b));
-        cursor += sizeof(color.b);
-    }
-}
 
 TEST_CASE( "Layout test", "[typeMapping]" )
 {
