3262786e0b6c7de22f9531f7d576133e50a8b78e
SemionV <semen.varennik@verivox.com>
2023-11-24 15:48:24 +0100

    Generic Lambda trait factory

--- src/baseTests/templates.cpp
+++ src/baseTests/templates.cpp
@@ -133,4 +133,48 @@ namespace test_fold_exprexxions
         std::array<std::string_view, 5> container = {"String1", "String2", "String3", "String4", "String5"};
         printArguments(container, Indices<0, 1, 2>());
     }
+
+    // helper: checking validity of \T\TI{f}(\TI{args}...)} for F \TI{f} and Args...
+    // \TI{args:
+    template <typename F, typename... Args,
+            typename = decltype(std::declval<F>()(std::declval<Args&&>()...))>
+    std::true_type isValidImpl(void*);
+
+    // fallback if helper SFINAE'd out:
+    template <typename F, typename... Args>
+    std::false_type isValidImpl(...);
+
+    // define a lambda that takes a lambda f and returns whether calling f with args
+    // is valid
+    inline constexpr auto isValid = [](auto f)
+    {
+        return [](auto&&... args)
+        {
+            return decltype(isValidImpl<decltype(f), decltype(args)&&...>(nullptr)){};
+        };
+    };
+
+    // helper template to represent a type as a value
+    template <typename T>
+    struct TypeT {
+        using Type = T;
+    };
+
+    // helper to wrap a type as a value
+    template <typename T>
+    constexpr auto type = TypeT<T>{};
+
+    // helper to unwrap a wrapped type in unevaluated contexts
+    template <typename T>
+    T valueT(TypeT<T>);  // no definition needed
+
+    constexpr auto isDefaultConstructible = isValid([](auto x) -> decltype((void)decltype(valueT(x))())
+    {
+    });
+
+    TEST_CASE( "Generic Lambda SFINAE", "[templates]" )
+    {
+        REQUIRE(isDefaultConstructible(type<int>));
+        REQUIRE(!isDefaultConstructible(type<int&>));
+    }
 }
