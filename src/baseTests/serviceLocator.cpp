8ce3547badc4db05ee51b00a5962d0bef61753ce
SemionV <hermit.sem@gmail.com>
2024-03-12 20:51:05 +0100

    Generalizing ServiceInstantiator

--- src/baseTests/serviceLocator.cpp
+++ src/baseTests/serviceLocator.cpp
@@ -20,33 +20,41 @@ struct ServiceInstantiator<TService>
     }
 };
 
-/*template<typename TService, typename... TDependencies>
-struct ServiceInstantiator
+template<typename TService, typename... TDependencies>
+struct ServiceInstantiator<std::shared_ptr<TService>, TDependencies...>
 {
     template<typename TServiceLocator>
-    static TService getInsatnce(TServiceLocator& services)
+    static std::shared_ptr<TService> getInsatnce(TServiceLocator& services)
     {
-        return TService{(services.template get<TDependencies>(), ...)};
+        return std::make_shared<TService>((services.template get<TDependencies>(), ...));
     }
-};*/
+};
+
+template<typename TService>
+struct ServiceInstantiator<std::shared_ptr<TService>>
+{
+    template<typename TServiceLocator>
+    static std::shared_ptr<TService> getInsatnce(TServiceLocator& services)
+    {
+        return std::make_shared<TService>();
+    }
+};
 
 template<typename TService, typename TServiceInstantiator = ServiceInstantiator<TService>>
 struct ServiceDependency
 {
     using ServiceType = TService;
-    using ServiceValueType = TService;
     using ServiceInstantiatorType = TServiceInstantiator;
 };
 
 template<typename TService, typename TServiceInstantiator>
 struct ServiceDependency<std::shared_ptr<TService>, TServiceInstantiator>
 {
-    using ServiceType = TService;
-    using ServiceValueType = std::shared_ptr<TService>;
+    using ServiceType = std::shared_ptr<TService>;
     using ServiceInstantiatorType = TServiceInstantiator;
 };
 
-template<typename TService, typename TServiceInstantiator>
+template<typename TService, typename TServiceInstantiator = ServiceInstantiator<TService>>
 struct TransientServiceDependency: ServiceDependency<TService, TServiceInstantiator>
 {
 };
@@ -54,14 +62,14 @@ struct TransientServiceDependency: ServiceDependency<TService, TServiceInstantia
 template<typename TDependency, typename TServiceLocator>
 struct DependencyController
 {
-    TDependency::ServiceValueType service;
+    TDependency::ServiceType service;
 
     explicit DependencyController(TServiceLocator& services):
             service(TDependency::ServiceInstantiatorType::template getInsatnce<TServiceLocator>(services))
     {}
 
 protected:
-    [[nodiscard]] decltype(auto) getInstance(TServiceLocator& services)
+    decltype(auto) getInstance(TServiceLocator& services)
     {
         return &service;
     }
@@ -70,17 +78,17 @@ protected:
 template<typename TService, typename TServiceInstantiator, typename TServiceLocator>
 struct DependencyController<ServiceDependency<std::shared_ptr<TService>, TServiceInstantiator>, TServiceLocator>
 {
-    using DependencyServiceValueType = ServiceDependency<std::shared_ptr<TService>, TServiceInstantiator>::ServiceValueType;
+    using DependencyServiceType = ServiceDependency<std::shared_ptr<TService>, TServiceInstantiator>::ServiceType;
     using DependencyServiceInstantiatorType = ServiceDependency<TService, TServiceInstantiator>::ServiceInstantiatorType;
 
-    DependencyServiceValueType service;
+    DependencyServiceType service;
 
     explicit DependencyController(TServiceLocator& services):
             service(DependencyServiceInstantiatorType::template getInsatnce<TServiceLocator>(services))
     {}
 
 protected:
-    [[nodiscard]] decltype(auto) getInstance(TServiceLocator& services)
+    decltype(auto) getInstance(TServiceLocator& services)
     {
         return service;
     }
@@ -93,9 +101,9 @@ struct DependencyController<TransientServiceDependency<TService, TServiceInstant
     {}
 
 protected:
-    [[nodiscard]] decltype(auto) getInstance(TServiceLocator& services)
+    decltype(auto) getInstance(TServiceLocator& services)
     {
-        return TServiceInstantiator::template getInsatnce<TService, TServiceLocator>(services);
+        return TServiceInstantiator::template getInsatnce<TServiceLocator>(services);
     }
 };
 
@@ -133,45 +141,47 @@ public:
 };
 
 using Service1Dependency = ServiceDependency<Service1>;
+using Service2TransientDependency = TransientServiceDependency<Service1, ServiceInstantiator<Service2, Service1Dependency>>;
 using Service2Dependency = ServiceDependency<Service2, ServiceInstantiator<Service2, Service1Dependency>>;
-
-template<typename TService>
-struct Service2PointerInstantiator
-{
-    template<typename TServiceLocator>
-    static decltype(auto) getInsatnce(TServiceLocator& services)
-    {
-        auto service1 = services.template get<Service1Dependency>();
-        auto service = std::make_shared<TService>(service1);
-        service->value = 3;
-        return service;
-    }
-};
-using Service2PointerDependency = ServiceDependency<std::shared_ptr<Service2>, Service2PointerInstantiator<Service2>>;
+using Service2PointerDependency = ServiceDependency<std::shared_ptr<Service2>, ServiceInstantiator<std::shared_ptr<Service2>, Service1Dependency>>;
 
 TEST_CASE("Check concept", "Service Locator")
 {
-    auto serviceLocator = ServiceLocator<Service1Dependency,
+    auto services = ServiceLocator<Service1Dependency,
                                         Service2Dependency,
-                                        Service2PointerDependency>{};
+                                        Service2PointerDependency,
+                                        Service2TransientDependency>{};
 
-    auto service1 = serviceLocator.get<Service1Dependency>();
+    auto service1 = services.get<Service1Dependency>();
     if(service1)
     {
         std::cout << "Service1 value: " << service1->value << "\n";
     }
 
-    auto service2 = serviceLocator.get<Service2Dependency>();
+    auto service2 = services.get<Service2Dependency>();
     if(service2)
     {
         std::cout << "Service2 value: " << service2->value << "\n";
         std::cout << "Service2->service1 value: " << service2->service1->value << "\n";
     }
 
-    auto service2Pointer = serviceLocator.get<Service2PointerDependency>();
+    auto service2Pointer = services.get<Service2PointerDependency>();
+    if(service2Pointer)
+    {
+        service2Pointer->value = 3;
+    }
+
+    service2Pointer = services.get<Service2PointerDependency>();
     if(service2Pointer)
     {
         std::cout << "Service2Pointer value: " << service2Pointer->value << "\n";
         std::cout << "Service2Pointer->service1 value: " << service2Pointer->service1->value << "\n";
     }
+
+    auto service2Transient = services.get<Service2TransientDependency>();
+    REQUIRE(service2Transient.value == 2);
+    service2Transient.value = 4;
+
+    service2Transient = services.get<Service2TransientDependency>();
+    REQUIRE(service2Transient.value == 2);
 }
