132d1764e6f4d30acd1c6602c0f703180ef45115
SemionV <hermit.sem@gmail.com>
2024-04-01 15:49:19 +0200

    Working on unix build

--- src/testApp/main.cpp
+++ src/testApp/main.cpp
@@ -1,8 +1,18 @@
 #include "dependencies.h"
 #include "project.h"
 
+#ifdef WIN32
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR szArgs, int nCmdShow)
 {
     auto project = testApp::Project{};
     return project.run();
 }
+#endif
+
+#ifdef __unix__
+int main()
+{
+    auto project = testApp::Project{};
+    return project.run();
+}
+#endif
