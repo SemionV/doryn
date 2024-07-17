a425ded21580285ba094dc9eeeee05c98b727eb3
SemionV <hermit.sem@gmail.com>
2024-07-17 21:25:31 +0200

    Sandbox extension

--- examples/sandbox/modules/extension/src/extension.cpp
+++ examples/sandbox/modules/extension/src/extension.cpp
@@ -4,12 +4,11 @@ namespace dory::sandbox
 {
     void Extension::attach(LibraryHandle library, ExtensionContext& moduleContext) {
         std::cout << "Attach extension\n";
-        /*auto& registry = moduleContext.registry;
+        auto& registry = moduleContext.registry;
         registry.services.scriptService.addScript("ping-extension", [&registry](Registry::DataContextType& context, const std::map<std::string, std::any>& arguments)
         {
-            registry.devices.terminalDevice.writeLine(fmt::format("-\u001B[31m{0}\u001B[0m-", "Hi :-) "));
-            //registry.events.applicationDispatcher.fire(context, events::application::Exit{});
-        });*/
+            registry.devices.terminalDevice.writeLine(fmt::format("\u001B[31m{0}\u001B[0m", "Hi :-) "));
+        });
     }
 
     Extension::~Extension() {
