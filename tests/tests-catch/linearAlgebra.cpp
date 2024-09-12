c03a191c9ffca92837bb359a94fac474411c97d8
SemionV <hermit.sem@gmail.com>
2024-09-12 19:05:34 +0200

    Move math code to a separate header-only library

--- tests/tests-catch/linearAlgebra.cpp
+++ tests/tests-catch/linearAlgebra.cpp
@@ -1,7 +1,7 @@
 #include <catch2/catch_test_macros.hpp>
 #include <iostream>
 
-#include <dory/engine/math/linearAlgebra.h>
+#include <dory/math/linearAlgebra.h>
 
 using namespace dory::math;
 
--- tests/tests-catch/matrixCalculatorTests.cpp
+++ tests/tests-catch/matrixCalculatorTests.cpp
@@ -1,7 +1,7 @@
 #include <catch2/catch_test_macros.hpp>
 
-#include <dory/engine/math/linearAlgebra.h>
-#include <dory/engine/math/trigonometry.h>
+#include <dory/math/linearAlgebra.h>
+#include <dory/math/trigonometry.h>
 
 using namespace dory::math;
 
