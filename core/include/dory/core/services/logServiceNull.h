39f4a2b137bcbeb0dccfc0cbe160408f2a434699
SemionV <hermit.sem@gmail.com>
2024-10-21 19:08:24 +0200

    Implement LogServiceNull with the generic implementation

--- core/include/dory/core/services/logServiceNull.h
+++ core/include/dory/core/services/logServiceNull.h
@@ -7,51 +7,12 @@
 
 namespace dory::core::services
 {
-    template<typename TInterface, typename TMessages>
-    class LogServiceBundle;
-
-    template<typename TMessage, typename TInterface, typename TMessages>
-    class LogServiceNullGeneric: public LogServiceBundle<TInterface, TMessages>
-    {
-    public:
-        inline void trace(const TMessage& message) final
-        {}
-
-        inline void debug(const TMessage& message) final
-        {}
-
-        inline void information(const TMessage& message) final
-        {}
-
-        inline void warning(const TMessage& message) final
-        {}
-
-        inline void error(const TMessage& message) final
-        {}
-
-        inline void critical(const TMessage& message) final
-        {}
-    };
-
-    template<typename TInterface, typename TMessage, typename... TMessages>
-    class LogServiceBundle<TInterface, generic::TypeList<TMessage, TMessages...>>: public LogServiceNullGeneric<TMessage, TInterface, generic::TypeList<TMessages...>>
-    {};
-
-    template<typename TInterface>
-    class LogServiceBundle<TInterface, generic::TypeList<>>: public TInterface
-    {};
-
-    class LogServiceNull: public LogServiceBundle<ILogService, ILogService::MessageTypes>
-    {};
-
     template<typename TMessage, typename TImplPolicy, typename TState>
-    class LogServiceNullGeneric2: public generic::implementation::ImplementationLevel<TImplPolicy, TState>
+    class LogServiceNullGeneric: public generic::implementation::ImplementationLevel<TImplPolicy, TState>
     {
     public:
         inline void trace(const TMessage& message) final
-        {
-            std::cout << message << std::endl;
-        }
+        {}
 
         inline void debug(const TMessage& message) final
         {}
@@ -69,5 +30,10 @@ namespace dory::core::services
         {}
     };
 
-    using LogServiceNull2 = generic::implementation::Implementation<generic::implementation::ImplementationPolicy<generic::TypeList<ILogService>, generic::implementation::ImplementationList<LogServiceNullGeneric2>, ILogService::MessageTypes>>;
+    using LogServiceNull = generic::implementation::Implementation<
+            generic::implementation::ImplementationPolicy<
+                    generic::TypeList<ILogService>,
+                    generic::implementation::ImplementationList<LogServiceNullGeneric>,
+                    ILogService::MessageTypes>
+                >;
 }
