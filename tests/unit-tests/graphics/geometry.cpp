3eb290921c12b9f587d49b720cf93c4551af8461
SemionV <hermit.sem@gmail.com>
2024-12-10 07:30:54 +0100

    Refactored Shape and other classes for geometry generation

--- tests/unit-tests/graphics/geometry.cpp
+++ tests/unit-tests/graphics/geometry.cpp
@@ -25,7 +25,14 @@ struct Point<T, 1>
 
     explicit Point(const T& _x): x(_x)
     {}
+
+    glm::vec1 toVec1() const
+    {
+        return glm::vec3{ x };
+    }
 };
+
+using Point1f = Point<float, 1>;
 
 template<typename T>
 struct Point<T, 2>: public Point<T, 1>
@@ -38,7 +45,14 @@ struct Point<T, 2>: public Point<T, 1>
             Point<T, 1>(_x),
             y(_y)
     {}
+
+    glm::vec2 toVec2() const
+    {
+        return glm::vec2{ this->x, y };
+    }
 };
+
+using Point2f = Point<float, 2>;
 
 template<typename T>
 struct Point<T, 3>: public Point<T, 2>
@@ -48,10 +62,17 @@ struct Point<T, 3>: public Point<T, 2>
     Point() = default;
 
     explicit Point(const T& _x, const T& _y, const T& _z):
-            Point<T, 1>(_x, _y),
+            Point<T, 2>(_x, _y),
             z(_z)
     {}
+
+    glm::vec3 toVec3() const
+    {
+        return glm::vec3{ this->x, this->y, z };
+    }
 };
+
+using Point3f = Point<float, 3>;
 
 struct Edge
 {
@@ -64,37 +85,47 @@ struct Face
 private:
     using EdgesType = std::deque<std::size_t>;
 
-    EdgesType edges;
+    EdgesType _edges;
 
 public:
     Face() = default;
 
     explicit Face(std::size_t edgesCount):
-            edges{edgesCount}
+            _edges{edgesCount}
     {}
 
-    EdgesType::const_iterator begin()
+    Face(std::initializer_list<std::size_t> edges):
+            _edges{edges}
+    {}
+
+    [[nodiscard]] EdgesType::const_iterator begin() const
     {
-        return edges.begin();
+        return _edges.begin();
     }
 
-    EdgesType::const_iterator end()
+    [[nodiscard]] EdgesType::const_iterator end() const
     {
-        return edges.end();
+        return _edges.end();
     }
 
     std::size_t addEdge(std::size_t edge)
     {
-        auto edgesCount = edges.size();
-        edges.push_back(edge);
+        auto edgesCount = _edges.size();
+        _edges.push_back(edge);
         return edgesCount;
     }
 
     std::size_t addEdgeInFront(std::size_t edge)
     {
-        auto edgesCount = edges.size();
-        edges.push_front(edge);
+        auto edgesCount = _edges.size();
+        _edges.push_front(edge);
         return edgesCount;
+    }
+
+    [[nodiscard]] std::size_t get(std::size_t edgeIndex) const
+    {
+        assert(edgeIndex >= 0 && edgeIndex < _edges.size());
+        return _edges[edgeIndex];
     }
 };
 
@@ -104,47 +135,74 @@ class Shape
 private:
     using PointType = Point<T, Dimensions>;
 
-    std::vector<PointType> points;
-    std::vector<Edge> edges;
-    std::vector<Face> faces;
+    std::vector<PointType> _points;
+    std::vector<Edge> _edges;
+    std::vector<Face> _faces;
 
 public:
     static constexpr std::size_t dimensions = Dimensions;
 
+    Shape() = default;
+
+    Shape(std::initializer_list<PointType> points, std::initializer_list<Edge> edges, std::initializer_list<Face> faces):
+        _points{points}, _edges{edges}, _faces{faces}
+    {}
+
     std::size_t getPointCount()
     {
-        return points.size();
+        return _points.size();
     }
 
     std::size_t getEdgeCount()
     {
-        return edges.size();
+        return _edges.size();
     }
 
     std::size_t getFaceCount()
     {
-        return faces.size();
+        return _faces.size();
     }
 
     std::size_t addPoint(const Point<T, Dimensions>& point)
     {
         auto pointCount = getPointCount();
-        points.emplace_back(point);
+        _points.emplace_back(point);
         return pointCount;
     }
 
     std::size_t addEdge(const Edge& edge)
     {
         auto edgeCount = getEdgeCount();
-        edges.emplace_back(edge);
+        _edges.emplace_back(edge);
         return edgeCount;
     }
 
     std::size_t addFace(const Face& face)
     {
         auto faceCount = getFaceCount();
-        faces.emplace_back(face);
+        _faces.emplace_back(face);
         return faceCount;
+    }
+
+    [[nodiscard]] const PointType& getPoint(std::size_t pointId) const
+    {
+        assert(pointId < _points.size() && pointId >= 0);
+
+        return _points[pointId];
+    }
+
+    [[nodiscard]] const Edge& getEdge(std::size_t edgeId) const
+    {
+        assert(edgeId < _edges.size()  && edgeId >= 0);
+
+        return _edges[edgeId];
+    }
+
+    [[nodiscard]] const Face& getFace(std::size_t faceId) const
+    {
+        assert(faceId < _faces.size() && faceId >= 0);
+
+        return _faces[faceId];
     }
 };
 
@@ -153,111 +211,47 @@ using Shape3d = Shape<T, 3>;
 
 struct Corner
 {
-    std::size_t left;
-    std::size_t center;
-    std::size_t right;
+    std::size_t left {};
+    std::size_t center {};
+    std::size_t right {};
 };
 
-template<typename T>
-std::size_t addPoint(glm::vec3 point, Shape3d<T>& shape)
+Corner getCorner(const Edge& edge1, const Edge& edge2)
 {
-    auto pointCount = shape.points.size() / Shape3d<T>::dimensions;
-
-    shape.points.push_back(point.x);
-    shape.points.push_back(point.y);
-    shape.points.push_back(point.z);
-
-    return pointCount;
-}
-
-template<typename T>
-std::size_t addEdge(std::size_t pointA, std::size_t pointB, Shape3d<T>& shape)
-{
-    auto edgeCount = shape.edges.size() / 2;
-
-    shape.edges.push_back(pointA);
-    shape.edges.push_back(pointB);
-
-    return edgeCount;
-}
-
-template<typename T>
-std::size_t addFace(Shape3d<T>& shape)
-{
-    auto faceCount = shape.faces.size();
-
-    shape.faces.emplace_back(typename Shape3d<T>::FaceType{});
-
-    return faceCount;
-}
-
-template<typename T>
-Shape3d<T>::FaceType& getFace(std::size_t face, Shape3d<T>& shape)
-{
-    assert(shape.faces.size() > face);
-
-    return shape.faces[face];
-}
-
-template<typename T>
-glm::vec3 getPoint(std::size_t point, const Shape3d<T>& shape)
-{
-    std::size_t vertexIndex = point * 3;
-    return glm::vec3{ shape.points[vertexIndex], shape.points[vertexIndex + 1], shape.points[vertexIndex + 2] };
-}
-
-template<typename T>
-std::size_t getEdgeBegin(std::size_t edge, const Shape3d<T>& shape)
-{
-    return shape.edges[edge * 2];
-}
-
-template<typename T>
-std::size_t getEdgeEnd(std::size_t edge, const Shape3d<T>& shape)
-{
-    return shape.edges[edge * 2 + 1];
-}
-
-template<typename T>
-Corner getCorner(std::size_t edge1, std::size_t edge2, const Shape3d<T>& shape)
-{
-    Corner corner {};
-
-    if(getEdgeBegin(edge1, shape) == getEdgeBegin(edge2, shape))
+    if(edge1.begin == edge2.begin)
     {
-        corner.left = getEdgeEnd(edge1, shape);
-        corner.center = getEdgeBegin(edge1, shape);
-        corner.right = getEdgeEnd(edge2, shape);
+        return Corner {edge1.end, edge1.begin, edge2.end};
     }
-    else if(getEdgeBegin(edge1, shape) == getEdgeEnd(edge2, shape))
+    else if(edge1.begin == edge2.end)
     {
-        corner.left = getEdgeEnd(edge1, shape);
-        corner.center = getEdgeBegin(edge1, shape);
-        corner.right = getEdgeBegin(edge2, shape);
+        return Corner {edge1.end, edge1.begin, edge2.begin};
     }
-    else if(getEdgeEnd(edge1, shape) == getEdgeBegin(edge2, shape))
+    else if(edge1.end == edge2.begin)
     {
-        corner.left = getEdgeBegin(edge1, shape);
-        corner.center = getEdgeEnd(edge1, shape);
-        corner.right = getEdgeEnd(edge2, shape);
+        return Corner {edge1.begin, edge1.end, edge2.end};
     }
-    else if(getEdgeEnd(edge1, shape) == getEdgeEnd(edge2, shape))
+    else if(edge1.end == edge2.end)
     {
-        corner.left = getEdgeBegin(edge1, shape);
-        corner.center = getEdgeEnd(edge1, shape);
-        corner.right = getEdgeBegin(edge2, shape);
+        return Corner {edge1.begin, edge1.end, edge2.begin};
     }
 
-    return corner;
+    return {};
 }
 
 template<typename T>
-glm::vec3 getNormal(std::size_t edge1, std::size_t edge2, const Shape3d<T>& shape)
+glm::vec3 getNormal(const Face& face, const Shape3d<T>& shape)
 {
-    Corner corner = getCorner(edge1, edge2, shape);
+    assert(std::distance(face.begin(), face.end()) > 1);
 
-    auto v1 = getPoint(corner.center, shape) - getPoint(corner.left, shape);
-    auto v2 = getPoint(corner.center, shape) - getPoint(corner.right, shape);
+    auto& edge1 = shape.getEdge(face.get(0));
+    auto& edge2 = shape.getEdge(face.get(1));
+
+    Corner corner = getCorner(edge1, edge2);
+
+    auto center = shape.getPoint(corner.center).toVec3();
+
+    auto v1 = center - shape.getPoint(corner.left).toVec3();
+    auto v2 = center - shape.getPoint(corner.right).toVec3();
 
     return glm::cross(v2, v1);
 }
@@ -265,7 +259,7 @@ glm::vec3 getNormal(std::size_t edge1, std::size_t edge2, const Shape3d<T>& shap
 template<typename T>
 Shape3d<T> push(const std::size_t faceId, glm::vec3 direction, const Shape3d<T>& shape)
 {
-    Shape3d<T> resultShape {shape};
+    /*Shape3d<T> resultShape {shape};
 
     assert(faceId < resultShape.faces.size());
 
@@ -334,7 +328,7 @@ Shape3d<T> push(const std::size_t faceId, glm::vec3 direction, const Shape3d<T>&
         ++i;
     }
 
-    return resultShape;
+    return resultShape;*/
 }
 
 template<typename T>
@@ -354,28 +348,23 @@ TEST(Geometry, triangularPrism)
     auto width = 8.f;
 
     auto triangularPrismBase = Shape3d<float>{
-            {width,-width,0, width,0,0, 0,0,0},
-            {0,1, 1,2, 2,0},
-            { {0, 1, 2} }
+            { Point3f{ width, -width, 0 }, Point3f{ width, 0, 0 }, Point3f{ 0, 0, 0 } },
+            { Edge{ 0, 1 }, Edge{ 1, 2 }, Edge{ 2, 0 }},
+            { Face{ 0, 1, 2 } }
     };
 
-    auto normal = glm::normalize(getNormal(0, 1, triangularPrismBase));
+    auto& face = triangularPrismBase.getFace(0);
+    auto normal = glm::normalize(getNormal(face, triangularPrismBase));
     EXPECT_EQ(normal, glm::vec3(0, 0, 1));
 
-    normal = glm::normalize(getNormal(1, 2, triangularPrismBase));
-    EXPECT_EQ(normal, glm::vec3(0, 0, 1));
-
-    normal = glm::normalize(getNormal(2, 0, triangularPrismBase));
-    EXPECT_EQ(normal, glm::vec3(0, 0, 1));
-
-    auto triangularPrism = push(0, math::Vector3f{ 0, 0, -8 }, triangularPrismBase);
+    /*auto triangularPrism = push(0, math::Vector3f{ 0, 0, -8 }, triangularPrismBase);
     EXPECT_EQ(triangularPrism.faces.size(), 5);
     auto& oppositeFace = triangularPrism.faces[1];
     EXPECT_EQ(oppositeFace.size(), 3);
     normal = glm::normalize(getNormal(oppositeFace[0], oppositeFace[1], triangularPrism));
     EXPECT_EQ(normal, glm::vec3(0, 0, -1));
 
-    assertPrism(triangularPrism);
+    assertPrism(triangularPrism);*/
 
     //auto mesh = buildMesh(triangularPrism);
 }
