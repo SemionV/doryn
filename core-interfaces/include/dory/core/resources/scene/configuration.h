525ee951f3300ff1913fbeab383fd5c64d9c7be4
SemionV <hermit.sem@gmail.com>
2025-02-12 11:40:06 +0100

    Working on adaptive layout concept

--- core-interfaces/include/dory/core/resources/scene/configuration.h
+++ core-interfaces/include/dory/core/resources/scene/configuration.h
@@ -86,17 +86,47 @@ namespace dory::core::resources::scene::configuration
         GraphicalSystem graphicalSystem;
     };
 
+    namespace layout
+    {
+        struct Dimension
+        {
+            std::optional<int> pixels{};
+            std::optional<float> percents{};
+        };
+
+        struct Position
+        {
+            std::optional<Dimension> x{};
+            std::optional<Dimension> y{};
+        };
+
+        struct Size
+        {
+            std::optional<Dimension> width{}; //if it has no value use content width
+            std::optional<Dimension> height{}; //if it has no value use content height
+        };
+
+        struct Container;
+
+        struct Container
+        {
+            std::string name;
+            std::optional<Position> position {};
+            std::optional<Size> size {}; //if it has no value use container's available space
+            std::vector<Container> containers;
+        };
+    }
+
     struct Window
     {
-        std::size_t width;
-        std::size_t height;
+        layout::Container container;
         std::string title;
         std::string graphicalContext;
     };
 
     struct View
     {
-        
+        std::string container; //attach to a grid column
     };
 
     struct SceneConfiguration: Entity<>
@@ -112,5 +142,87 @@ namespace dory::core::resources::scene::configuration
         std::vector<factory::Instance<services::ISceneDirector>> directors;
         std::unordered_map<std::string, GraphicalContext> graphicalContexts {};
         std::unordered_map<std::string, Window> windows {};
+
+        //fullscreen window with a centered box
+        Window window1 = {
+            {
+                "window1",
+                std::optional<layout::Position> {}, //auto, stick to the left upper corner of the available space: {0,0}
+                std::optional<layout::Size> {}, //auto: use all available screen space
+                {
+                    layout::Container{
+                        "dialog",
+                        layout::Position {}, //auto: centered rectangle according to rectangle size and available space
+                        layout::Size { layout::Dimension{800}, layout::Dimension{600} }
+                    }
+                }
+            }
+        };
+
+        //relatively placed window of flexible height with a grid layout
+        Window window2 = {
+            {
+                "window2",
+                layout::Position {
+                    layout::Dimension{100}/*100px offset from the left*/,
+                    {}/*no value - centered by height of the screen*/
+                },
+                layout::Size { layout::Dimension{1024}, layout::Dimension{}/*Use height of the children*/},
+                {
+                    {
+                        "row1", {}, layout::Size{ {}/*available width of the parent*/, layout::Dimension{100} },
+                        {
+                            { "cell_11", {}, layout::Size{ layout::Dimension{{}, {40.3f}}, {}/*height of the parent*/ }},
+                            { "cell_12", {}, {} },
+                            { "cell_13", {}, {} }
+                        }
+                    },
+                    {
+                        "row2", {}, layout::Size{ {}/*available width of the parent*/, layout::Dimension{100} },
+                        {
+                            { "cell_21", {}, layout::Size{ layout::Dimension{{}, {40.3f}}, {}/*height of the parent*/ }},
+                            { "cell_22", {}, {} },
+                            { "cell_23", {}, {} }
+                        }
+                    },
+                    {
+                        "row3", {}, layout::Size{ {}/*available width of the parent*/, layout::Dimension{100} },
+                        {
+                            { "cell_31", {}, layout::Size{ layout::Dimension{{}, {40.3f}}, {}/*height of the parent*/ }},
+                            { "cell_32", {}, {} },
+                            { "cell_33", {}, {} }
+                        }
+                    }
+                }
+            }
+        };
+
+        Window window3 = {
+        };
+
+        Window window4 = {
+            {
+                "window4",
+                layout::Position {}, //auto: centered
+                layout::Size {}, //auto: size of content
+                {
+                    {
+                        "container with vertical scroll",
+                        std::optional<layout::Position> {}, //auto, stick to the left upper corner of the available space: {0,0}
+                        layout::Size { layout::Dimension{1024}, layout::Dimension{768} },
+                        {
+                            {
+                                "very tall content container",
+                                std::optional<layout::Position> {},
+                                layout::Size {
+                                    {}/*use width of the parent if children do not provide fixed size*/,
+                                    layout::Dimension{3000} /*taller than the parent, reason for scrolls*/
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        };
     };
 }
