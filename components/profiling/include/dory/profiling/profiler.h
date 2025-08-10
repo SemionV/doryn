a981a6877de842a952ea534f2bc394cc655279e7
SemionV <hermit.sem@gmail.com>
2025-08-10 15:06:29 +0200

    Separate GPU profiling option from general profiling

--- components/profiling/include/dory/profiling/profiler.h
+++ components/profiling/include/dory/profiling/profiler.h
@@ -7,8 +7,9 @@
 #ifdef DORY_PROFILING_ON
 #include <tracy/Tracy.hpp>
 #include <tracy/TracyC.h>
+#endif
 
-//TODO: separate GPU profiling from CPU profiling
+#ifdef DORY_GPU_PROFILING_ON
 #ifndef DORY_OPENGL_INCLUDED
 #include <glad/gl.h>
 #define DORY_OPENGL_INCLUDED
@@ -29,6 +30,9 @@ namespace dory::profiling
     DORY_DLLEXPORT void traceAllocation(const void* ptr, std::size_t size, const char* poolName);
     DORY_DLLEXPORT void traceDeallocation(const void* ptr, const char* poolName);
     DORY_DLLEXPORT void shutdown();
+#endif
+
+#ifdef DORY_GPU_PROFILING_ON
     DORY_DLLEXPORT void registerOpenGLContext(const char* contextName, std::size_t nameSize);
     DORY_DLLEXPORT void collectGpuTraces();
 #endif
@@ -70,12 +74,6 @@ name, __FUNCTION__, __FILE__, __LINE__, 0 \
 
 #define DORY_TRACE_ZONE_END(varName) varName.end();
 
-#define DORY_TRACE_GPU_ZONE(name) \
-static const tracy::SourceLocationData DORY_CONCAT(_tracyGpuLoc_, __LINE__) = { \
-name, __FUNCTION__, __FILE__, static_cast<uint32_t>(__LINE__), 0 \
-}; \
-tracy::GpuCtxScope DORY_CONCAT(_tracyGpuZone_, __LINE__)(&DORY_CONCAT(_tracyGpuLoc_, __LINE__), TRACY_CALLSTACK, true)
-
 #define DORY_TRACE_FRAME_MARK dory::profiling::traceFrameMark();
 #define DORY_TRACE_FRAME_START(name) dory::profiling::traceFrameStart(name);
 #define DORY_TRACE_FRAME_END(name) dory::profiling::traceFrameEnd(name);
@@ -84,8 +82,6 @@ tracy::GpuCtxScope DORY_CONCAT(_tracyGpuZone_, __LINE__)(&DORY_CONCAT(_tracyGpuL
 #define DORY_TRACE_MEM_FREE(ptr, poolName) dory::profiling::traceDeallocation(ptr, poolName);
 #define DORY_TRACE_START() dory::profiling::startProfiler();
 #define DORY_TRACE_SHUTDOWN() dory::profiling::stopProfiler();
-#define DORY_TRACE_GPU_CONTEXT(name, size) dory::profiling::registerOpenGLContext(name, size);
-#define DORY_TRACE_GPU_COLLECT() dory::profiling::collectGpuTraces();
 
 #else
 
@@ -100,6 +96,21 @@ tracy::GpuCtxScope DORY_CONCAT(_tracyGpuZone_, __LINE__)(&DORY_CONCAT(_tracyGpuL
 #define DORY_TRACE_MEM_FREE(ptr, poolName) DORY_NOOP;
 #define DORY_TRACE_START() DORY_NOOP;
 #define DORY_TRACE_SHUTDOWN() DORY_NOOP;
+
+#endif
+
+#ifdef DORY_GPU_PROFILING_ON
+#define DORY_TRACE_GPU_ZONE(name) \
+static const tracy::SourceLocationData DORY_CONCAT(_tracyGpuLoc_, __LINE__) = { \
+name, __FUNCTION__, __FILE__, static_cast<uint32_t>(__LINE__), 0 \
+}; \
+tracy::GpuCtxScope DORY_CONCAT(_tracyGpuZone_, __LINE__)(&DORY_CONCAT(_tracyGpuLoc_, __LINE__), TRACY_CALLSTACK, true)
+
+#define DORY_TRACE_GPU_CONTEXT(name, size) dory::profiling::registerOpenGLContext(name, size);
+#define DORY_TRACE_GPU_COLLECT() dory::profiling::collectGpuTraces();
+
+#else
+
 #define DORY_TRACE_GPU_CONTEXT(name, size) DORY_NOOP;
 #define DORY_TRACE_GPU_COLLECT() DORY_NOOP;
 #define DORY_TRACE_GPU_ZONE(name) DORY_NOOP;
