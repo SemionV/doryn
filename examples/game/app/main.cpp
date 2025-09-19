25ef5fa2740a452577ed378a01942271381b6039
Semion Varennik <semion.varennik@verivox.com>
2025-09-19 16:47:40 +0300

    Add trace zones to the game::main function

--- examples/game/app/main.cpp
+++ examples/game/app/main.cpp
@@ -71,27 +71,36 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR szArgs, int nCmdShow)
     std::cout << "End Start Profiler" << std::endl;
 
     {
+        DORY_TRACE_ZONE_NAMED(zoneBuildCoreObject, "game::main - Build Core Objects");
         dory::game::Setup setup;
         dory::generic::extension::LibraryHandle staticLibraryHandle {};
         auto registry = dory::core::Registry{};
         auto configuration = dory::core::resources::configuration::Configuration{};
         auto localization = dory::core::resources::LocalizationImpl{};
         auto context = dory::core::resources::DataContext{ configuration, localization };
+        DORY_TRACE_ZONE_END(zoneBuildCoreObject);
 
+        DORY_TRACE_ZONE_NAMED(zoneSetupRegistry, "game::main - Setup Registry");
         setup.setupRegistry(staticLibraryHandle, registry, configuration);
+        DORY_TRACE_ZONE_END(zoneSetupRegistry);
 
         //initial config
+        DORY_TRACE_ZONE_NAMED(zoneConfigureBootLogger, "game::main - Configure Boot Logger");
         configuration.section.loadFrom.emplace_back("settings.yaml");
         auto& bootLoggerConfig = configuration.loggingConfiguration.configurationLogger;
         bootLoggerConfig.name = "boot";
         bootLoggerConfig.rotationLogger = dory::core::resources::configuration::RotationLogSink{"logs/boot.log"};
         bootLoggerConfig.stdoutLogger = dory::core::resources::configuration::StdoutLogSink{};
+        DORY_TRACE_ZONE_END(zoneConfigureBootLogger);
 
+        DORY_TRACE_ZONE_NAMED(zoneBootstrapInitialize, "game::main - Bootstrap Initialize");
         auto bootstrap = dory::game::Bootstrap{registry};
         bootstrap.initialize(staticLibraryHandle, context);
+        DORY_TRACE_ZONE_END(zoneBootstrapInitialize);
 
         dory::core::resources::scene::Scene* rootScene;
 
+        DORY_TRACE_ZONE_NAMED(zoneBuildScene, "game::main - Build Scene");
         if(auto sceneConfigurationService = registry.get<dory::core::services::ISceneConfigurationService>())
         {
             dory::core::resources::scene::configuration::SceneConfiguration sceneConfig;
@@ -102,12 +111,18 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR szArgs, int nCmdShow)
                 rootScene = sceneBuilder->build(sceneConfig, context);
             }
         }
+        DORY_TRACE_ZONE_END(zoneBuildScene);
 
+        DORY_TRACE_ZONE_NAMED(zoneGameInitialize, "game::main - Game Initialize");
         auto game = dory::game::Game{ registry };
         game.initialize(staticLibraryHandle, context);
+        DORY_TRACE_ZONE_END(zoneGameInitialize);
 
+        DORY_TRACE_ZONE_NAMED(zoneBootstrapRun, "game::main - Bootstrap Run");
         bootstrap.run(context);
+        DORY_TRACE_ZONE_END(zoneBootstrapRun);
 
+        DORY_TRACE_ZONE_NAMED(zoneDestroyScene, "game::main - Destroy Scene");
         if(rootScene)
         {
             if(auto sceneBuilder = registry.get<dory::core::services::ISceneBuilder>())
@@ -115,8 +130,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR szArgs, int nCmdShow)
                 sceneBuilder->destroy(*rootScene, context);
             }
         }
+        DORY_TRACE_ZONE_END(zoneDestroyScene);
 
+        DORY_TRACE_ZONE_NAMED(zoneBootstrapCleanup, "game::main - Bootstrap Cleanup");
         bootstrap.cleanup(context);
+        DORY_TRACE_ZONE_END(zoneBootstrapCleanup);
     }
 
     std::cout << "Begin Shutdown Profiler" << std::endl;
