1de4dc6fa194847ea8e4fe7f2f9e332488245c74
SemionV <hermit.sem@gmail.com>
2025-11-17 16:26:43 +0100

    Working on custom dynamic List implementation

--- components/containers/include/dory/containers/list.h
+++ components/containers/include/dory/containers/list.h
@@ -35,17 +35,58 @@ namespace dory::containers
             _allocator(allocator)
         {}
 
-        BasicList(size_type count, const T& value, allocator_type& allocator);
+        BasicList(const size_type count, const T& value, allocator_type& allocator):
+            _allocator(allocator)
+        {
+            _data = static_cast<T*>(_allocator.allocate(count * sizeof(T)));
+            _size = count;
+            _capacity = count;
 
-        explicit BasicList(size_type count, allocator_type& allocator);
+            for (size_type i = 0; i < count; ++i)
+            {
+                ::new (static_cast<void*>(&_data[i])) T(value);
+            }
+        }
+
+        explicit BasicList(const size_type count, allocator_type& allocator):
+            _allocator(allocator)
+        {
+            _data = static_cast<T*>(_allocator.allocate(count * sizeof(T)));
+            _size = count;
+            _capacity = count;
+        }
 
         BasicList(const BasicList&) = delete;  // stateful allocators forbid copying
 
-        BasicList(BasicList&& other) noexcept;
+        BasicList(BasicList&& other) noexcept:
+            _allocator(other._allocator),
+            _data(other._data),
+            _size(other._size),
+            _capacity(other._capacity)
+        {
+            other._size = 0;
+            other._capacity = 0;
+            other._data = nullptr;
+        }
 
-        BasicList& operator=(BasicList&& other) noexcept;
+        BasicList& operator=(BasicList&& other) noexcept:
+            _allocator(other._allocator)
+        {
+            _data = other._data;
+            _size = other._size;
+            _capacity = other._capacity;
 
-        ~BasicList();
+            other._size = 0;
+            other._capacity = 0;
+            other._data = nullptr;
+
+            return *this;
+        }
+
+        ~BasicList()
+        {
+            _allocator.deallocate(_data, _capacity * sizeof(T));
+        }
 
 
     // ----------------------------------------------------------
@@ -110,9 +151,7 @@ namespace dory::containers
         // Performs raw memory reallocation + moves/copies elements
         void reallocate(const size_type newCap)
         {
-            assert::inhouse(newCap > _capacity, "New capacity must be larger than current capacity");
-
-            const size_type oldSize = _size;
+            assert::inhouse(newCap >= _size, "Existing data will not fit into the new buffer");
 
             // Allocate new block
             T* newData = static_cast<T*>(_allocator.allocate(newCap * sizeof(T)));
@@ -121,7 +160,7 @@ namespace dory::containers
             if constexpr (std::is_nothrow_move_constructible_v<T>)
             {
                 // Best path: no-throw move
-                for (size_type i = 0; i < oldSize; ++i)
+                for (size_type i = 0; i < _size; ++i)
                 {
                     new (&newData[i]) T(std::move(_data[i]));
                 }
@@ -129,7 +168,7 @@ namespace dory::containers
             else if constexpr (std::is_copy_constructible_v<T>)
             {
                 // Fallback: copy if move potentially throws
-                for (size_type i = 0; i < oldSize; ++i)
+                for (size_type i = 0; i < _size; ++i)
                 {
                     new (&newData[i]) T(_data[i]);
                 }
@@ -139,12 +178,11 @@ namespace dory::containers
                 static_assert(std::is_move_constructible_v<T>, "BasicList<T> requires T to be move-constructible or copy-constructible.");
             }
 
+            destroy_range(0, _size);
+
             // Deallocate old memory
             if (_data)
-            {
-                destroy_range(0, oldSize);
                 _allocator.deallocate(_data, _capacity * sizeof(T));
-            }
 
             // Install new block
             _data = newData;
@@ -155,16 +193,47 @@ namespace dory::containers
         {
             if constexpr (!std::is_trivially_destructible_v<T>)
             {
+                if (begin >= end)
+                    return; // empty range, nothing to do
+
+                assert::inhouse(_data != nullptr, "destroy_range called with null data pointer");
+
                 for (size_type i = begin; i < end; ++i)
                     _data[i].~T();
             }
         }
 
         // Construction helpers
-        void construct_at(size_type index, const T& value);
-        void construct_at(size_type index, T&& value);
+        void construct_at(size_type index, const T& value)
+        {
+            assert::inhouse(index < _capacity, "Index out of bounds");
+            new (&_data[index]) T(value);
+        }
+
+        template<typename U>
+        void construct_at(size_type index, U&& value)
+        {
+            assert::inhouse(index < _capacity, "Index out of bounds");
+
+            if constexpr (std::is_trivially_constructible_v<T, U&&>)
+            {
+                // For trivial types we do not need placement new.
+                _data[index] = static_cast<U&&>(value);
+            }
+            else
+            {
+                // Proper placement-new construction
+                ::new (static_cast<void*>(&(_data[index]))) T(static_cast<U&&>(value));
+            }
+        }
 
         template<typename... Args>
-        void construct_at_emplace(size_type index, Args&&... args);
+        void construct_at_emplace(size_type index, Args&&... args)
+        {
+            assert::inhouse(index < _capacity, "Index out of bounds");
+
+            ::new (static_cast<void*>(&_data[index]))
+                T(std::forward<Args>(args)...);
+        }
     };
 }
