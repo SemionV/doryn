4a14501a575e85666bc4d184bf335ebea37fa2b5
SemionV <semion.varennik@gmail.com>
2026-02-09 05:52:11 +0100

    Working on event system

--- core-generic/include/dory/core/eventSystem.h
+++ core-generic/include/dory/core/eventSystem.h
@@ -1,37 +1,31 @@
 #pragma once
 
-#include <atomic>
 #include <dory/data-structures/function.h>
+#include <dory/data-structures/containers/lockfree/freeListArray.h>
 
 namespace dory::core::event_system
 {
-    template<typename TKey = std::size_t>
-    class EventHandlerKeyGenerator
-    {
-        static_assert(std::atomic<TKey>::is_always_lock_free, "HandlerKeyType atomic is not lock-free on this platform. Pick another numeric type.");
-
-    public:
-        using HandlerKeyType = TKey;
-
-    private:
-        std::atomic<HandlerKeyType> _currentKey { 1 }; //0 can be used as sentinel
-
-    public:
-        HandlerKeyType generate()
-        {
-            return _currentKey.fetch_add(1, std::memory_order::relaxed);
-        }
-    };
+    using IndexType = std::uint32_t;
 
     struct EventHandlerKey
     {
-
+        IndexType index;
+        IndexType generation;
     };
 
-    template<typename TKeyGenerator, class... TParams>
+    template<typename... TParams>
     class Event
     {
-    public:
+    private:
+        using ListType = data_structures::containers::lockfree::freelist::FreeListArray<Payload, AllocatorType, 32, 1048576>;
 
+    public:
+        using HandlerType = data_structures::function::Function<void(TParams...)>;
+
+        template<typename F>
+        EventHandlerKey attachHandler(F&& handler)
+        {
+
+        }
     };
 }
