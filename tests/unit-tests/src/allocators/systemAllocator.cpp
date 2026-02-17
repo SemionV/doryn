cd9ff9abc5dd2f428fdcf8739270a7f9a288e6db
SemionV <semion.varennik@gmail.com>
2026-02-17 19:32:31 +0100

    Allocate object with non-trivial contructor/destructor unit test

--- tests/unit-tests/src/allocators/systemAllocator.cpp
+++ tests/unit-tests/src/allocators/systemAllocator.cpp
@@ -3,14 +3,47 @@
 
 #include <dory/memory/allocators/general-purpose/systemAllocator.h>
 
-//TODO: check if constructor is called
 //TODO: check if destructor is called
 
-TEST(SystemAllocatorTests, simpleAllocation)
+TEST(SystemAllocatorTests, allocateObject)
 {
     dory::memory::allocators::general_purpose::SystemAllocator allocator { nullptr };
 
     int* obj = allocator.allocateObject<int>({});
 
     EXPECT_TRUE(obj);
+}
+
+struct TestType
+{
+    int mem1 = 0;
+    bool& destructed;
+
+    TestType(bool& destructed):
+        destructed(destructed)
+    {
+        mem1 = 1;
+    }
+
+    ~TestType()
+    {
+        destructed = true;
+    }
+};
+
+TEST(SystemAllocatorTests, allocateAndConstructObject)
+{
+    dory::memory::allocators::general_purpose::SystemAllocator allocator { nullptr };
+
+    bool destructed = false;
+    {
+        const auto obj = allocator.allocateObject<TestType>({}, destructed);
+
+        EXPECT_TRUE(obj);
+        EXPECT_EQ(obj->mem1, 1);
+
+        allocator.deallocateObject(obj);
+    }
+
+    EXPECT_TRUE(destructed);
 }
